e1a9f9f1e62676f21a0e0e8b6a57ea7716976acf
dinhmai74 <dinh.mai@nextfunc.com>
2019-04-10 13:55:31 +0700

    add semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
-import HomeScreen from 'containers/home-screen';
-import React, { Component } from 'react';
-import { Trans } from 'react-i18next';
-import './App.css';
-import logo from './logo.svg';
+import { Hero, Navbar } from '@front10/landing-page-book/dist/components'
+import HomeScreen from 'containers/home-screen'
+import React, { Component } from 'react'
+import { Trans } from 'react-i18next'
+import './App.css'
+import './semantic/dist/semantic.min.css';
+import logo from './logo.svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> <code />
-          </p>
-          <Trans>welcome</Trans>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <HomeScreen/>
-        </header>
+        <Navbar
+          brandLink="https://front10.com"
+          brandLogo="https://front10.com/img/logos/logo.png"
+        />
+        <Hero
+          backgroundColor="#212529"
+          particlesParams={{ particles: { line_linked: { enable: false } } }}
+          header="Landing Page Book"
+          subHeader="Some cool text"
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/stories/index.js
+++ src/components/stories/index.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React from "react"
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+import { linkTo } from "@storybook/addon-links"
+import styled from 'styled-components'
 
-import { Button, Welcome } from "@storybook/react/demo";
+import { Welcome } from "@storybook/react/demo"
+import "semantic/dist/semantic.min.css"
+import { Button } from "semantic-ui-react";
+
 
 storiesOf("Welcome", module).add("to Storybook", () => (
   <Welcome showApp={linkTo("Button")} />
-));
+))
 
 storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
+  .add("with text", () => <Button primary onClick={action("clicked")}>Hello Button</Button>)
   .add("with some emoji", () => (
     <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
-  ));
+  ))
