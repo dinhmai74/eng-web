d58e631cc0c4da72cff147db5e62b3cdca101c8c
dinhmai74 <dinh.mai@nextfunc.com>
2019-04-11 21:48:06 +0700

    add bootstrap

--- src/containers/home-screen/components/MainMenu.tsx
+++ src/containers/home-screen/components/MainMenu.tsx
@@ -51,10 +51,8 @@ class MainMenu extends Component<IProps, IState> {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown title={tran('whyChooseUs')} id="basic-nav-dropdown">
+              <NavDropdown.Item href={strings.routeReasonChoseUs}>{tran('reasonChooseUs')}</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
