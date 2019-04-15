0e0eae384f6132d048bcc97113cf6e402385d2b6
haulinh <linh.le@nextfunc.com>
2019-04-15 23:25:01 +0700

    add reason page

--- src/components/stories/index.js
+++ src/components/stories/index.js
@@ -1,40 +1,42 @@
-import React from "react"
+import React from "react";
+import { tran } from "localization/i18n";
 
-import { action } from "@storybook/addon-actions"
-import { linkTo } from "@storybook/addon-links"
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 import {
   withKnobs,
   text,
   select,
   boolean,
   object,
-  array,
-} from "@storybook/addon-knobs"
-import { storiesOf } from "@storybook/react"
-import styled from "styled-components"
-import 'semantic/dist/semantic.min.css';
-import './menu.story'
+  array
+} from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import styled from "styled-components";
+import "semantic/dist/semantic.min.css";
+import "./menu.story";
 
-
-import { Welcome } from "@storybook/react/demo"
-import { Button,SemanticCOLORS, } from "semantic-ui-react"
+import { Welcome } from "@storybook/react/demo";
+import { Button, SemanticCOLORS } from "semantic-ui-react";
 import { oneOf } from "prop-types";
+import ReasonChoseUsScreen from "containers/why-chose-us/reason-chose-us-screen/index";
+
+storiesOf("Reason", module).add("to Storybook", () => <ReasonChoseUsScreen />);
 
 storiesOf("Welcome", module).add("to Storybook", () => (
   <Welcome showApp={linkTo("Button")} />
-))
+));
 
-const themes = ["primary", "secondary"]
-const colors: SemanticCOLORS[]= ["red","black","blue","brown","green"]
+const themes = ["primary", "secondary"];
+const colors: SemanticCOLORS[] = ["red", "black", "blue", "brown", "green"];
 
 storiesOf("Button", module)
   .addDecorator(withKnobs)
   .add("with theme", () => (
     <React.Fragment>
-     <Button >Default</Button>
-     <button className="ui button primary">Primary</button>
-     <Button secondary>secondary</Button>
-
+      <Button>Default</Button>
+      <button className="ui button primary">Primary</button>
+      <Button secondary>secondary</Button>
     </React.Fragment>
   ))
   .add("playground", () => (
@@ -43,8 +45,8 @@ storiesOf("Button", module)
       loading={boolean("loading", false)}
       big={boolean("big", true)}
       style={object("style", {})}
-      color={select("color",colors,"red")}
+      color={select("color", colors, "red")}
     >
       {text("label", "Hello Button")}
     </Button>
-  ))
+  ));
--- src/containers/why-chose-us/reason-chose-us-screen/index.tsx
+++ src/containers/why-chose-us/reason-chose-us-screen/index.tsx
@@ -1,4 +1,9 @@
+import ReasonCard from 'components/ReasonCard/index'
+import { tran } from 'localization/i18n'
 import React, { PureComponent } from 'react'
+import { Header } from 'semantic-ui-react'
+
+import './styles.css'
 
 interface IProps {}
 
@@ -8,7 +13,81 @@ class ReasonChoseUsScreen extends PureComponent<IProps, State> {
   readonly state: State = {}
 
   render() {
-    return <div>Reason chose us</div>
+    return (
+      <div className="container">
+        <h1 className="container-header">
+          Choose Wall Street English right now
+        </h1>
+        <p className="container-header__content">
+          There are lots of English teaching centers, but Wall Street English is
+          the only one designed for busy adults just like you.
+        </p>
+        <ReasonCard
+          isFirstItem
+          reason={tran('reason1')}
+          title={tran('tileReason1')}
+          content={tran('contentR1')}
+          ref1="http://wallstreetenglish.edu.vn/en/our-method.html"
+          ref2="http://wallstreetenglish.edu.vn/en/our-method/our-certificates.html"
+          contentRef1={tran('contentRef1R1')}
+          contentRef2={tran('contentRef2R1')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          reason={tran('reason2')}
+          title={tran('tileReason2')}
+          content={tran('contentR2')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          reason={tran('reason3')}
+          title={tran('tileReason3')}
+          content={tran('contentR3')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          reason={tran('reason4')}
+          title={tran('tileReason4')}
+          content={tran('contentR4')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          reason={tran('reason5')}
+          title={tran('tileReason5')}
+          content={tran('contentR5')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          reason={tran('reason6')}
+          title={tran('tileReason6')}
+          content={tran('contentR6')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          reason={tran('reason7')}
+          title={tran('tileReason7')}
+          content={tran('contentR7')}
+          image={require('themes/images/reason-1.png')}
+        />
+
+        <ReasonCard
+          isLastItem
+          reason={tran('reason8')}
+          title={tran('tileReason8')}
+          content={tran('contentR8')}
+          ref="http://wallstreetenglish.edu.vn/en/our-method/our-certificates.html"
+          contentRef={tran('contentRefR8')}
+          image={require('themes/images/reason-1.png')}
+        />
+        <Header as="h2">Choose Wall Street English right now</Header>
+      </div>
+    )
   }
 }
 
