919a910412564cb01948ef606b30397532d8fb8e
haulinh <linh.le@nextfunc.com>
2019-04-16 10:56:43 +0700

    add images

--- src/containers/why-chose-us/reason-chose-us-screen/index.tsx
+++ src/containers/why-chose-us/reason-chose-us-screen/index.tsx
@@ -3,6 +3,7 @@ import { tran } from 'localization/i18n'
 import React, { PureComponent } from 'react'
 import { Header } from 'semantic-ui-react'
 
+import { images } from 'themes/index'
 import './styles.css'
 
 interface IProps {}
@@ -31,49 +32,49 @@ class ReasonChoseUsScreen extends PureComponent<IProps, State> {
           ref2="http://wallstreetenglish.edu.vn/en/our-method/our-certificates.html"
           contentRef1={tran('contentRef1R1')}
           contentRef2={tran('contentRef2R1')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason1}
         />
 
         <ReasonCard
           reason={tran('reason2')}
           title={tran('tileReason2')}
           content={tran('contentR2')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason2}
         />
 
         <ReasonCard
           reason={tran('reason3')}
           title={tran('tileReason3')}
           content={tran('contentR3')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason3}
         />
 
         <ReasonCard
           reason={tran('reason4')}
           title={tran('tileReason4')}
           content={tran('contentR4')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason4}
         />
 
         <ReasonCard
           reason={tran('reason5')}
           title={tran('tileReason5')}
           content={tran('contentR5')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason5}
         />
 
         <ReasonCard
           reason={tran('reason6')}
           title={tran('tileReason6')}
           content={tran('contentR6')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason6}
         />
 
         <ReasonCard
           reason={tran('reason7')}
           title={tran('tileReason7')}
           content={tran('contentR7')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason7}
         />
 
         <ReasonCard
@@ -83,7 +84,7 @@ class ReasonChoseUsScreen extends PureComponent<IProps, State> {
           content={tran('contentR8')}
           ref="http://wallstreetenglish.edu.vn/en/our-method/our-certificates.html"
           contentRef={tran('contentRefR8')}
-          image={require('themes/images/reason-1.png')}
+          image={images.reason8}
         />
         <Header as="h2">Choose Wall Street English right now</Header>
       </div>
