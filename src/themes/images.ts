554c625515f3693aaa4fe6250a2718f5801dc8b7
dinhmai74 <dinh.mai@nextfunc.com>
2019-04-17 21:22:37 +0700

    change path image

--- src/themes/images.ts
+++ src/themes/images.ts
@@ -1,19 +1,19 @@
 // tslint:disable: object-literal-sort-keys
 export const images = {
-  reason1: require('../../public/asset/images/why-choose-wse/8Reason/reason-1.png'),
-  reason2: require('../../public/asset/images/why-choose-wse/8Reason/reason-2.png'),
-  reason3: require('../../public/asset/images/why-choose-wse/8Reason/reason-3.png'),
-  reason4: require('../../public/asset/images/why-choose-wse/8Reason/reason-4.png'),
-  reason5: require('../../public/asset/images/why-choose-wse/8Reason/reason-5.png'),
-  reason6: require('../../public/asset/images/why-choose-wse/8Reason/reason-6.png'),
-  reason7: require('../../public/asset/images/why-choose-wse/8Reason/reason-7.png'),
-  reason8: require('../../public/asset/images/why-choose-wse/8Reason/reason-8.png'),
+  reason1: '/asset/images/why-choose-wse/8Reason/reason-1.png',
+  reason2: '/asset/images/why-choose-wse/8Reason/reason-2.png',
+  reason3: '/asset/images/why-choose-wse/8Reason/reason-3.png',
+  reason4: '/asset/images/why-choose-wse/8Reason/reason-4.png',
+  reason5: '/asset/images/why-choose-wse/8Reason/reason-5.png',
+  reason6: '/asset/images/why-choose-wse/8Reason/reason-6.png',
+  reason7: '/asset/images/why-choose-wse/8Reason/reason-7.png',
+  reason8: '/asset/images/why-choose-wse/8Reason/reason-8.png',
 
-  icon1: require('../../public/asset/images/why-choose-wse/UniqueMethod/icon-1.png'),
-  icon2: require('../../public/asset/images/why-choose-wse/UniqueMethod/icon-2.png'),
-  icon3: require('../../public/asset/images/why-choose-wse/UniqueMethod/icon-3.png'),
-  icon4: require('../../public/asset/images/why-choose-wse/UniqueMethod/icon-4.png'),
-  icon5: require('../../public/asset/images/why-choose-wse/UniqueMethod/icon-5.png'),
-  icon6: require('../../public/asset/images/why-choose-wse/UniqueMethod/icon-6.png'),
-  footer: require('../../public/asset/images/why-choose-wse/UniqueMethod/footer.png')
+  icon1: '/asset/images/why-choose-wse/UniqueMethod/icon-1.png',
+  icon2: '/asset/images/why-choose-wse/UniqueMethod/icon-2.png',
+  icon3: '/asset/images/why-choose-wse/UniqueMethod/icon-3.png',
+  icon4: '/asset/images/why-choose-wse/UniqueMethod/icon-4.png',
+  icon5: '/asset/images/why-choose-wse/UniqueMethod/icon-5.png',
+  icon6: '/asset/images/why-choose-wse/UniqueMethod/icon-6.png',
+  footer: '/asset/images/why-choose-wse/UniqueMethod/footer.png'
 }
