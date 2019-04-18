f4d6f3c344ffc5b230cfb583f5416be5877d1c4a
thanhloc17 <loc.do@nextfunc.com>
2019-04-18 16:37:13 +0700

    finish screen finder course step 1

--- src/themes/images/index.ts
+++ src/themes/images/index.ts
@@ -1,4 +1,6 @@
 export const images = {
   general: require('assets/images/general.jpg'),
   preparation: require('assets/images/preparation.jpg'),
+  bgFindCourse: require('assets/images/bg.jpg'),
+  logoFindCourse: require('assets/images/logo.png'),
 }
