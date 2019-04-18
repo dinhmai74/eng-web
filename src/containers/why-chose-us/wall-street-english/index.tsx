cb1d91af25cd0cd76d40d4231d8706538fdfd5b0
haulinh <linh.le@nextfunc.com>
2019-04-18 10:25:51 +0700

    semi done WallStreet

--- src/containers/why-chose-us/wall-street-english/index.tsx
+++ src/containers/why-chose-us/wall-street-english/index.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react'
-import { Container } from 'react-bootstrap'
 import { Box, Flex } from 'rebass'
+import { Divider } from 'semantic-ui-react'
 import { images } from 'themes'
+
+import { tran } from 'localization/i18n'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin: auto 400px auto 400px;
+`
+
+const H2 = styled.h2`
+  color: #003c78;
+`
+
+const P = styled.p`
+  font-size: 20px;
+`
+
+const LeftContainer = styled.div`
+  border-left: 1px solid #38546d;
+  /* height: 80px; */
+  /* position: absolute;
+  right: 249px;
+  top: 10px; */
+`
 
 export interface IProps {}
 
@@ -11,22 +34,14 @@ export default class WallStreetEnglish extends React.PureComponent<
 > {
   render() {
     return (
-      <div
-        style={{
-          marginLeft: 400,
-          marginRight: 400
-        }}
-      >
+      <Container>
         <Flex>
           <Flex width={1 / 3} justifyContent="center">
             <img src={images.img1} />
           </Flex>
           <Box width={2 / 3}>
-            <h2>Make more of life through learning</h2>
-            <p>
-              Everyone is born with a dream. At Wall Street English, we help you
-              to achieve yours...
-            </p>
+            <H2>{tran('titleWallStreet1')}</H2>
+            <P>{tran('contentWallStreet1')}</P>
           </Box>
         </Flex>
 
@@ -37,9 +52,9 @@ export default class WallStreetEnglish extends React.PureComponent<
             width={1 / 2}
             bg="#003C78"
           >
-            <Box>
-              <Box>DREAM ABOUT</Box>
-              <Box>STUDYING ABROAD</Box>
+            <Box marginRight={3} color="white" fontSize={3}>
+              <Box>{tran('topWallStreet2')}</Box>
+              <Box marginTop={3}>{tran('bottomWallStreet2')}</Box>
             </Box>
           </Flex>
           <Box width={1 / 2} bg="white">
@@ -52,20 +67,33 @@ export default class WallStreetEnglish extends React.PureComponent<
 
         <Flex>
           <Flex width={1 / 2}>
-            <Box>
+            <Box width={1 / 2}>
               <img style={{ height: '100%' }} src={images.img3} />
             </Box>
-            <Flex alignItems="center" justifyContent="center" bg="#03A9F4">
-              DREAM ABOUT A BETTER CAREER
+            <Flex
+              width={1 / 2}
+              alignItems="center"
+              justifyContent="center"
+              bg="#03A9F4"
+              color="white"
+              fontSize={3}
+            >
+              <Box marginLeft={3}>{tran('leftWallStreet3')}</Box>
             </Flex>
           </Flex>
 
           <Flex width={1 / 2}>
-            <Box>
+            <Box width={1 / 2}>
               <img style={{ height: '100%' }} src={images.img4} />
             </Box>
-            <Flex alignItems="center" justifyContent="center" bg="#8BC34A">
-              DREAM ABOUT TRAVELLING THE WORLD
+            <Flex
+              alignItems="center"
+              justifyContent="center"
+              bg="#8BC34A"
+              color="white"
+              fontSize={3}
+            >
+              <Box marginLeft={3}>{tran('rightWallStreet3')}</Box>
             </Flex>
           </Flex>
         </Flex>
@@ -74,11 +102,11 @@ export default class WallStreetEnglish extends React.PureComponent<
           <Box width={1 / 2}>
             <img src={images.img5} />
           </Box>
-          <Flex width={1 / 2} alignItems="center">
-            DREAM ABOUT TRAVELLING THE WORLD
+          <Flex width={1 / 2} alignItems="center" color="black" fontSize={3}>
+            <Box marginLeft={3}>{tran('WallStreet4')}</Box>
           </Flex>
         </Flex>
-      </div>
+      </Container>
     )
   }
 }
