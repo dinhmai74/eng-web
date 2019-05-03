8a73ada6589b3b13f6ba9dbcf96f3dd5e6c8013b
haulinhlk <linh.le@nextfunc.com>
2019-05-03 08:47:09 +0700

    save code find course

--- src/containers/find-course-screens/feeling-step/FeelingStep.tsx
+++ src/containers/find-course-screens/feeling-step/FeelingStep.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
+import { steps } from './questions/steps.js'
+import MultiStep from 'react-multistep'
 
-interface IFeelingStepProps {
-}
+interface IFeelingStepProps {}
 
-interface IFeelingStepState {
-}
+interface IFeelingStepState {}
 
 class FeelingStep extends Component<IFeelingStepProps, IFeelingStepState> {
   static defaultProps = {}
@@ -14,6 +14,7 @@ class FeelingStep extends Component<IFeelingStepProps, IFeelingStepState> {
     return (
       <div>
         <p>FeelingStep screen</p>
+        <MultiStep steps={steps} />
       </div>
     )
   }
