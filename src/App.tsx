b1c41b194f09aa842ea68bc5b28df7c02b40ccf3
haulinh <haulinhlk@gmail.com>
2019-05-06 13:05:15 +0700

    fix path router

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App = (props) => {
           </Route>
         ))}
         <Route path="/test-result" component={TestResult} />
-        <Route path="/result" component={TestResult} />
+        <Route path="/result" component={Result} />
       </div>
       <InformationForm />
       <Footer />
