b68e99c972bfeff1d5fd70f79b5d7d754a75ea24
ThanhLoc <dothanhloc1998@gmail.com>
2019-06-14 10:42:24 +0700

    add screen speaking ielts beginner

--- src/containers/reading-ielts-beginners/Question/type.ts
+++ src/containers/reading-ielts-beginners/Question/type.ts
@@ -5,4 +5,4 @@ export interface IQuestion {
   d: string
 }
 
-export type TQuestion = 'reading' | 'writting'
+export type TQuestion = 'reading' | 'writting' | 'speaking'
---
+++ src/containers/speaking-ielts-beginners/TotalStepSpeaking.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import _ from 'lodash'
+import { images } from 'themes'
+
+import { Wizard } from '@front10/landing-page-book/dist/components'
+import TitleQuestion from 'containers/reading-ielts-beginners/title-question/TitleQuestion'
+import stepsDataSpeaking from './stepsDataSpeaking.json'
+import Question from 'components/QuestionIelts/Question.tsx'
+import { controls } from 'react-media-player'
+import QuestionAudio from 'components/QuestionAudio/QuestionAudio.tsx'
+const { PlayPause, MuteUnmute } = controls
+
+export interface IState {
+  play: boolean
+  pause: boolean
+}
+
+const Url = _.map(stepsDataSpeaking, (e) => ({
+  component: (
+    <Question
+      questionNumber={e.number}
+      type={e.type}
+      images1={images.iconWriting}
+      images2={images.iconListening}
+      images3={images.iconSpeaking}
+    />
+  )
+}))
+
+const resultStep = _.map(stepsDataSpeaking, (e) => ({
+  name: <TitleQuestion questionNumber={e.number} type={e.type} />,
+  component: <QuestionAudio questionNumber={e.number} type={e.type} />
+}))
+
+export default class TotalStep extends React.Component<any, IState> {
+  render() {
+    return (
+      <Wizard
+        contentAlign="left"
+        backButtonText="Prev"
+        steps={resultStep}
+        prevBtnOnLastStep={false}
+      />
+    )
+  }
+}
