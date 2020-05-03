39f5b5791bcc17d7bd193a832af825918e8f9cb4
dinhmai74 <anh.dinh74@gmail.com>
2020-05-03 20:50:41 +0700

    add detail

--- project/src/components/text/Text.tsx
+++ project/src/components/text/Text.tsx
@@ -5,14 +5,66 @@ import React from "react"
 interface TextProps {
   className?: string
   text?: string
+  children?: string
 }
 
 export const Text: React.FC<TextProps> = ({ className, children, text }) => {
-  let tx: any
+  let tx: string
   if (text) tx = tran(text)
   else {
-    tx = typeof children === "string" ? tran(children) : children
+    tx = tran(children)
   }
 
-  return <p className={cx("text-gray-800 leading-loose", className)}>{tx}</p>
+  const txArr = tx.split("\n")
+
+  if (txArr.length <= 1) return <p className={cx("text-gray-800 leading-loose", className)}>{tx}</p>
+
+  return (
+    <>
+      {txArr.map((v: string, i) => {
+        if (v === "") return <br />
+        const strongRex = /<b>(.*?)<\/b>/g
+        // const strongTags = .exec(v)
+        // const results: any[] = []
+        // const stringsRs: string[] = []
+        // let cloneString = v
+        // // while ((match = strongRex.exec(v))) {
+        // results.push(match)
+        // stringsRs.push(cloneString.slice(lastIndex, match.index))
+        // cloneString = cloneString.slice(match.index + match[0].length, cloneString.length)
+        // }
+        const strongResults = v.split(strongRex)
+        // console.log("abc", abc)
+
+        // let time = 0
+        // while (cloneString.length > 0 && time < 10) {
+        // const match = strongRex.exec(cloneString)
+        // if (match) {
+        // results.push(match)
+        // stringsRs.push(cloneString.slice(0, match.index))
+        // cloneString = cloneString
+        // .slice(match.index + match[0].length, cloneString.length)
+        // .toString()
+        // } else {
+        // // console.log("match", match)
+        // stringsRs.push(cloneString)
+        // console.log("cloneString", cloneString)
+        // // cloneString = ""
+        // }
+        // time++
+        // }
+        // console.log("stringsRs", stringsRs)
+        // console.log("results[0]", results)
+
+        return (
+          <p className={cx("text-gray-800 leading-loose", className)} key={i}>
+            {strongResults.map((text, i) => {
+              if (i % 2 !== 0) return <b>{text} </b>
+              return text
+            })}
+          </p>
+        )
+      })}
+    </>
+  )
 }
--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -2,6 +2,93 @@
 const plugin = require("tailwindcss/plugin")
 module.exports = {
   theme: {
+    textStyles: (theme) => ({
+      // defaults to {}
+      heading: {
+        output: false, // this means there won't be a "heading" component in the CSS, but it can be extended
+        fontWeight: theme("fontWeight.bold"),
+        lineHeight: theme("lineHeight.tight"),
+      },
+      h1: {
+        extends: "heading", // this means all the styles in "heading" will be copied here; "extends" can also be an array to extend multiple text styles
+        fontSize: theme("fontSize.5xl"),
+        "@screen sm": {
+          fontSize: theme("fontSize.6xl"),
+        },
+      },
+      h2: {
+        extends: "heading",
+        fontSize: theme("fontSize.4xl"),
+        "@screen sm": {
+          fontSize: theme("fontSize.5xl"),
+        },
+      },
+      h3: {
+        extends: "heading",
+        fontSize: theme("fontSize.4xl"),
+      },
+      h4: {
+        extends: "heading",
+        fontSize: theme("fontSize.3xl"),
+      },
+      h5: {
+        extends: "heading",
+        fontSize: theme("fontSize.2xl"),
+      },
+      h6: {
+        extends: "heading",
+        fontSize: theme("fontSize.xl"),
+      },
+      link: {
+        fontWeight: theme("fontWeight.bold"),
+        color: theme("colors.blue.400"),
+        "&:hover": {
+          color: theme("colors.blue.600"),
+          textDecoration: "underline",
+        },
+      },
+      richText: {
+        fontWeight: theme("fontWeight.normal"),
+        fontSize: theme("fontSize.base"),
+        lineHeight: theme("lineHeight.relaxed"),
+        "> * + *": {
+          marginTop: "1em",
+        },
+        h1: {
+          extends: "h1",
+        },
+        h2: {
+          extends: "h2",
+        },
+        h3: {
+          extends: "h3",
+        },
+        h4: {
+          extends: "h4",
+        },
+        h5: {
+          extends: "h5",
+        },
+        h6: {
+          extends: "h6",
+        },
+        ul: {
+          listStyleType: "disc",
+        },
+        ol: {
+          listStyleType: "decimal",
+        },
+        a: {
+          extends: "link",
+        },
+        "b, strong": {
+          fontWeight: theme("fontWeight.bold"),
+        },
+        "i, em": {
+          fontStyle: "italic",
+        },
+      },
+    }),
     extend: {
       colors: {
         primary: {
@@ -115,8 +202,29 @@ module.exports = {
     transitionTimingFunction: ["responsive", "important"],
     transitionDuration: ["responsive", "important"],
     transitionDelay: ["responsive", "important"],
+    textIndent: ["responsive", "important"],
+    textShadow: ["responsive", " important"],
+    ellipsis: ["responsive", "important"],
+    hyphens: ["responsive", "important"],
+    kerning: ["responsive", "important"],
+    textUnset: ["responsive", "important"],
+    fontVariantCaps: ["responsive", "important"],
+    fontVariantNumeric: ["responsive", "important"],
+    fontVariantLigatures: ["responsive", "important"],
+    textRendering: ["responsive", "important"],
   },
   plugins: [
+    require("tailwindcss-bg-alpha")(),
+    require("@ky-is/tailwindcss-plugin-width-height")({ variants: ["responsive", "important"] }),
+    require("tailwindcss-typography")({
+      // all these options default to the values specified here
+      ellipsis: true, // whether to generate ellipsis utilities
+      hyphens: true, // whether to generate hyphenation utilities
+      kerning: true, // whether to generate kerning utilities
+      textUnset: true, // whether to generate utilities to unset text properties
+      componentPrefix: "t-", // the prefix to use for text style classes
+    }),
+
     plugin(function ({ addVariant }) {
       addVariant("important", ({ container }) => {
         container.walkRules((rule) => {
@@ -190,4 +298,5 @@ module.exports = {
   corePlugins: {
     display: false,
   },
+  alphaColors: ["red.500", "gray.200"],
 }
